5659398a0feddba6a0307fdb023f1eb073f121f2
Ragini Pandey <pandeyragini55@gmail.com>
2026-01-24 18:39:43 +0530

    Resume update

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
     "I am a Full-stack developer with a demonstrated history of working in the information technology and product development industry",
   github: "https://github.com/ragini-pandey",
   resume:
-    "https://drive.google.com/file/d/1Yb9O40q1dlDJl1HLfoY2liuv3avKsvn4/view?usp=sharing",
+    "https://drive.google.com/file/d/1Jj4_0MHz6zIY7RwhfXlo5MclZDfth_8J/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/ragini-pandey-dev/",
   twitter: "https://twitter.com/pandeyragini24",
   insta: "https://www.instagram.com/pandeyragini2407/",
